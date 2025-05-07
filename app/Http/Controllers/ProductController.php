b0cd76fe3d450c212c942a6d8ed3cea5f1b49a44
Pedro <projasma2002@gmail.com>
2025-05-06 22:48:17 -0500

    Add store method in ProductController with ProductService dependency

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductResource;
 use App\Models\Product;
+use App\Services\ProductService;
+use Exception;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    private ProductService $productService;
+
+    public function __construct(ProductService $productService){
+        $this->productService = $productService;
+    }
     /**
      * Display a listing of the resource.
      */
@@ -26,9 +35,23 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductRequest $request)
     {
-        //
+        $validatedData = $request->validated();
+
+        try {
+            $product = $this->productService->createProduct($validatedData);
+
+            return response()->json([
+                'message' => 'Product created successfully',
+                'product' => new ProductResource($product)
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'message' => 'An error occurred while creating the category',
+                'error' => $e->getMessage()
+            ], 500);
+        }
     }
 
     /**
