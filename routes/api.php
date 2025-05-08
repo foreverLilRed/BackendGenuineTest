b8ca67915f5f78b37dacf48dcef0791fc35dc9e5
Pedro <projasma2002@gmail.com>
2025-05-07 21:55:21 -0500

    Add final RESTful routes for product controller

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,10 @@ Route::prefix('category')->group(function(){
 });
 
 Route::prefix('product')->group(function(){
+    Route::get('/',[ProductController::class,'index']);
     Route::post('/',[ProductController::class,'store']);
+    Route::get('/{id}', [ProductController::class, 'show']); 
+    Route::put('/{id}', [ProductController::class, 'update']); 
+    Route::patch('/{id}', [ProductController::class, 'update']); 
+    Route::delete('/{id}', [ProductController::class, 'destroy']);
 });
