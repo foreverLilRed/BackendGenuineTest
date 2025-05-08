62bd3b7bda7fd5db2ad1ae409a2cb0cd32d0f732
Pedro <projasma2002@gmail.com>
2025-05-07 21:51:55 -0500

    Create ProductCollection to handle paginated product data responses

---
+++ app/Http/Resources/ProductCollection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class ProductCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @return array<int|string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'data' => ProductResource::collection($this->collection),
+            'meta' => [
+                'current_page' => $this->currentPage(),
+                'per_page' => $this->perPage(),
+                'total' => $this->total(),
+                'last_page' => $this->lastPage(),
+            ],
+            'links' => [
+                'self' => $this->url($this->currentPage()),
+                'first' => $this->url(1),
+                'last' => $this->url($this->lastPage()),
+                'prev' => $this->previousPageUrl(),
+                'next' => $this->nextPageUrl(),
+            ],
+        ];
+    }
+}
